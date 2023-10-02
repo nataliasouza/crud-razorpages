ea306a16641a02bba3a13bb72cd8c8352b44406f
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-10-02 14:11:55 -0300

    Adicionei a classe Premium e os Data Annotations

--- RazorTest/Models/Premium.cs
+++ RazorTest/Models/Premium.cs
@@ -1,6 +1,32 @@
-﻿namespace RazorTest.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace RazorTest.Models;
 
 public class Premium
 {
+    [Key]
+    [DisplayName("Id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o título do Premium")]
+    [StringLength(80, ErrorMessage = "O título deve conter até 80 caracteres")]
+    [MinLength(5, ErrorMessage = "O título deve conter pelo menos 5 caracteres")]
+    [DisplayName("Título")]
+    public string Título { get; set; } = string.Empty;
+
+    [DataType(DataType.DateTime)]
+    [DisplayName("Início")]
+    public DateTime StartDate { get; set; }
+
+    [DataType(DataType.DateTime)]
+    [DisplayName("Término")]
+    public DateTime EndDate { get; set; }
+
+    [DisplayName("Aluno")]
+    [Required(ErrorMessage = "Aluno inválido")]
+    public int StudentId { get; set; }
+
+    public Student? Student { get; set; }
 
 }
