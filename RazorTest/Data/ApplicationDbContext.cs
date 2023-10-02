6085909c5cf87129fd2d71a23e382f8ebf239436
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-10-02 16:29:36 -0300

    Gerei migrations e banco

--- RazorTest/Data/ApplicationDbContext.cs
+++ RazorTest/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RazorTest.Models;
 
 namespace RazorTest.Data;
 
@@ -9,4 +10,7 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Premium> Premiums { get; set; }
 }
