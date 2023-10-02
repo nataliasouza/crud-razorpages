163a7c52bdb93752a9143c373abbeecb6440e350
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-10-02 17:13:58 -0300

    Página de estudantes básica

---
+++ RazorTest/Pages/Students/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorTest.Data;
+using RazorTest.Models;
+
+namespace RazorTest.Pages_Students
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RazorTest.Data.ApplicationDbContext _context;
+
+        public DeleteModel(RazorTest.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Student Student { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Student = student;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FindAsync(id);
+
+            if (student != null)
+            {
+                Student = student;
+                _context.Students.Remove(Student);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorTest/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorTest.Data;
+using RazorTest.Models;
+
+namespace RazorTest.Pages_Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorTest.Data.ApplicationDbContext _context;
+
+        public IndexModel(RazorTest.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Students != null)
+            {
+                Student = await _context.Students.ToListAsync();
+            }
+        }
+    }
+}
